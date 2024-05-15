2310077aa5bcc253ac86bc1eec4ee511850146cb
lai2-24 <lai2-24@itismeucci.com>
2024-05-15 13:20:47 +0200

    amen

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
 
 function App() {
+  const [gameId, setGameId] = useState(null);
+  const [targetNumber, setTargetNumber] = useState(null);
+  const [guess, setGuess] = useState('');
+  const [feedback, setFeedback] = useState('');
+  const [attempts, setAttempts] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  async function startNewGame(){
+    const response = await fetch('http://localhost:8080/partita', {
+      method: 'POST'
+    });
+    const data = await response.json();
+    setGameId(data.id);
+    setTargetNumber(data.numero);
+    setFeedback('');
+    setAttempts(0);
+    setGuess('');
+  };
+  async function handleGuess(){
+    const response = await fetch(`http://localhost:8080/partita/${gameId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ numero: parseInt(guess) })
+    });
+    setLoading(true);
+    const data = await response.json();
+    setAttempts(data.tentativi);
+    if (data.risultato === 0) {
+      setFeedback('Congratulazioni! Hai indovinato il numero!');
+    } else if (data.risultato === -1) {
+      setFeedback('Il numero è troppo piccolo.');
+    } else {
+      setFeedback('Il numero è troppo grande.');
+    }
+    setLoading(false);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Indovina il numero</h1>
+      {gameId ? (
+        <div>
+          <p>ID Partita: {gameId}</p>
+          <p>Tentativi: {attempts}</p>
+          <input
+            type="number"
+            value={guess}
+            onChange={(e) => setGuess(e.target.value)}
+          />
+          {!loading ? (
+            <div>
+              <button onClick={handleGuess}>
+                Indovina
+              </button>
+            </div>
+          ) :(
+            <div>
+              <p>Caricamento...</p>
+            </div>
+          )}
+          <p>{feedback}</p>
+          <button onClick={startNewGame}>Inizia una nuova partita</button>
+        </div>
+      ) : (
+        <div>
+        <p>Clicca sul pulsante per iniziare a giocare!</p>
+        <button onClick={startNewGame}>Inizia una nuova partita</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
